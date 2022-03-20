ca52f0c4d2d85d60cbb5c90369b0152e2543e674
KingCraXy <cobergage@gmail.com>
2022-03-20 01:13:15 -0500

    Add files via upload

---
+++ cheats.c
@@ -0,0 +1,29 @@
+﻿void	__Space_Shooter__(void)
+{
+}
+
+void	Score_Hack(void)
+{
+	offset = 0x08000000;
+	WRITEU32(offset + 0x0125D80, 0x3B9AC9FF);
+	offset = 0;
+	data = 0;
+}
+
+void	__(void)
+{
+}
+
+void	__Color_Switch__(void)
+{
+}
+
+void	Score_Hack(void)
+{
+	offset = 0x08000000;
+	WRITEU32(offset + 0x012EF90, 0x00000063);
+	offset = 0;
+	data = 0;
+}
+
+void	__(void)
